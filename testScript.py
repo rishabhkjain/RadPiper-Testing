39e6ee348c6ae17de1f095bc32b9b74cc7d4cf7b
rishabhkjain <rishabh@rishabhkjain.com>
2019-03-30 15:29:15 -0400

    syntax fix

--- testScript.py
+++ testScript.py
@@ -4,22 +4,19 @@ from subprocess import call
 
 def testFile(topFolder, testIDLst, outputFieldsLst, fileNameLst, pathToCSV):
 	solutionLst = [] #where to store output files
-	call(cd " topFolder", shell=True) #cd to main directory
+	call('cd ' + topFolder, shell=True) #cd to main directory
 	for testID in testIDLst: #loop through the testIDs
 		tmpSolution = []
-		call(cp -a " /" + topFolder + "/" + testID + "/." + " /" + topFolder + "/mainBag/", shell=True)# copy all the files from the testID directory to mainBag
-		call(python "run_postprocess_batch.py " + pathToCSV + " " + "/" + topFolder + "/mainBag/ "+  "True", shell=True) #run the test file
+		call("cp -a  /" + topFolder + "/" + testID + "/." + " /" + topFolder + "/mainBag/", shell=True)# copy all the files from the testID directory to mainBag
+		call("python run_postprocess_batch.py " + pathToCSV + " " + "/" + topFolder + "/mainBag/ "+  "True", shell=True) #run the test file
 		for file in fileNameLst:#copy over and delete specific files
-			call (cp " /" + topFolder + "/mainBag/" + file + " /topFolder/" + testID + "/" + file, shell = True)
-			call (rm -rf +  " /" + topFolder + "/mainBag/" + file, shell = True)
-		for outputField in outputFieldLst:
+			call ("cp /" + topFolder + "/mainBag/" + file + " /" + topFolder + "/" + testID + "/" + file, shell = True)
+			call ("rm -rf  /" + topFolder + "/mainBag/" + file, shell = True)
+		for outputField in outputFieldsLst:
 			tmpSolution.append((outputField, PostProcessOutputArray[0][outputField]))
-
-	#save output list as csv
-	with open(resultFile, "w") as output:
-    	writer = csv.writer(output, lineterminator='\n')
+	with open("resultFile", "w") as output:
+		writer = csv.writer(output, lineterminator='\n')
     	writer.writerows(solutionLst)
-
 
 
 
